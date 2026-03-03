be6ccdbfdd5d8c3e31d842e5300dedfc42619bb5
Tayyab MAqsood <tybmaq123@gmail.com>
2026-03-03 12:37:31 +0500

    Sorting

---
+++ Sorting.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[1000];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+
+
+
+    for (int i = 0; i < n - 1; i++) {
+        int small = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[small])
+                small = j;
+        }
+
+
+        if (small != i) {
+            int temp = arr[i];
+            arr[i] = arr[small];
+            arr[small] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
